03010c22b1b4379d39e15ad208d328ef55eaf7b8
Yuze Fu <git@xfox.me>
2018-03-26 16:04:42 +0800

    better subscription view

--- src/Components/Subscription.tsx
+++ src/Components/Subscription.tsx
@@ -6,12 +6,15 @@ import * as Interfaces from '../lib/api_interfaces';
 const ColorHash = require('color-hash');
 const color = new ColorHash();
 import { RouterStore } from 'mobx-react-router';
+import Timeage from 'timeago.js';
+var timeago = Timeage();
 
 import * as Forms from '../Forms';
 import * as Components from '../Components';
 
-import { Card, Icon, Avatar, Button, message } from 'antd';
+import { Card, Icon, Avatar, Button, Collapse, Input, message } from 'antd';
 const { Meta } = Card;
+const { Panel } = Collapse;
 
 interface Props {
   id: string;
@@ -45,17 +48,34 @@ class Subscription extends React.Component<Props, States> {
   render() {
     const { subscription, service } = this.state;
     return (
-      <Card title={subscription && subscription.id}>
-        <Meta
-          avatar={<Avatar
-            style={{ backgroundColor: color.hex(service && service.id) }}
-            icon="fork"
-          />}
-          title={service && service.name}
-          description={service && service.id}
-        />
-        <p>{subscription && subscription.config}</p>
-      </Card>
+      <Collapse bordered={false}>
+        <Panel
+          key={this.props.id}
+          header={subscription && subscription.id}
+        >
+          <Meta
+            avatar={<Avatar
+              style={{ backgroundColor: color.hex(service && service.id) }}
+              icon="fork"
+            />}
+            title={service && service.name}
+            description={service && service.id}
+          />
+          <div style={{ marginTop: '12px', marginBottom: '12px' }} />
+          <p>配置信息：</p>
+          <p>
+            <Input.TextArea
+              placeholder="配置"
+              autosize={{ minRows: 2 }}
+            >
+              {subscription && subscription.config}
+            </Input.TextArea>
+          </p>
+          <p><Button type="primary" disabled={true}>修改</Button></p>
+          <p>创建于：{subscription && timeago.format(subscription.created_at, 'zh_CN')}</p>
+          <p>最近修改：{subscription && timeago.format(subscription.updated_at, 'zh_CN')}</p>
+        </Panel>
+      </Collapse>
     );
   }
 }
