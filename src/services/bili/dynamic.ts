9dd3f10b6d7f2e93bd876cbd61222b5bdb9b0f77
Yuze Fu <git@xfox.me>
2018-05-26 07:01:55 +0800

    better output

--- src/services/bili/dynamic.ts
+++ src/services/bili/dynamic.ts
@@ -10,79 +10,189 @@ import { SANDRA_CRAWLER_UA } from "../../config";
 import Message from "../../message";
 // tslint:disable-next-line:no-var-requires
 const JSONbig = require("json-bigint");
+import { parse as parseCookie } from "cookie";
+// import log from "../../log";
+
+const CARD_TYPES = {
+  ALL: 268435455,
+  REPOST: 1, // done
+  PIC: 2, // done
+  WORD: 4, // done
+  VIDEO: 8, // done
+  CLIP: 16, // done
+  DRAMA: 32, // done
+  ARTICLE: 64, // done
+  LIVE: 128,
+  MUSIC: 256,
+  BANGUMI: 512, // done
+  NONE: 1024,
+  H5_SHARE: 2048,
+  HOT: 1e3,
+};
+
+const mapCardToContent = (type: number, card: any) => {
+  // tslint:disable:max-line-length
+  switch (type) {
+    case CARD_TYPES.VIDEO:
+      return `
+        <p>
+          <a href="https://space.bilibili.com/${card.owner.mid}/#/dynamic">${card.owner.name}</a>
+          ${card.title}
+        </p>
+        <p><a href="https://www.bilibili.com/video/av${card.aid}"><img referrerpolicy="no-referrer" src="${card.pic}"></a></p>
+        <p>${card.desc.split("\n").join("<br>")}</p>
+      `;
+    case CARD_TYPES.CLIP:
+      return `
+        <p>
+          <a href="https://space.bilibili.com/${card.user.uid}/#/dynamic">${card.user.name}</a>
+          ${card.item.description}
+        </p>
+        <p><a href="https://vc.bilibili.com/video/${card.item.id}"><img referrerpolicy="no-referrer" src="${card.item.cover.default}"></a></p>
+      `;
+    case CARD_TYPES.DRAMA:
+      return `
+        <p>${card.title} 第 ${card.new_ep.index} 集 - ${card.new_ep.index_title}</p>
+        <p><a href="${card.new_ep.url}"><img referrerpolicy="no-referrer" src="${card.new_ep.cover}"></a></p>
+      `;
+    case CARD_TYPES.BANGUMI:
+      return `
+        <p>${card.apiSeasonInfo.title} 第 ${card.index} 集 - ${card.index_title}</p>
+        <p><a href="${card.url}"><img referrerpolicy="no-referrer" src="${card.cover}"></a></p>
+      `;
+    case CARD_TYPES.ARTICLE:
+      return `
+        <p><a href="https://www.bilibili.com/read/cv${card.id}"><b>${card.title}</b></a></p>
+        <p><a href="https://space.bilibili.com/${card.author.mid}/#/article"></a></p>
+        ${card.banner_url ?
+          `<p><img referrerpolicy="no-referrer" src="${card.banner_url}"></p>
+          <p>${card.summary}</p>` :
+          `<p>${card.summary}</p>
+          <p>
+          ${(card.image_urls as string[]).map(image => `<img referrerpolicy="no-referrer" src="${image}">`).join("<br>")}
+          </p>`
+        }
+      `;
+    case CARD_TYPES.PIC:
+      return `
+        <p>
+          <a href="https://space.bilibili.com/${card.user.uid}/#/dynamic">${card.user.name}</a>：
+          ${card.item.description}
+        </p>
+        <p>
+        ${card.item.pictures.map((pic: any) => `<img referrerpolicy="no-referrer" src="${pic.img_src}">`).join("<br>")}
+        </p>
+        <p><a href="https://h.bilibili.com/${card.item.id}">阅读原文</a></p>
+      `;
+    case CARD_TYPES.WORD:
+      return `
+        <p><a href="https://space.bilibili.com/${card.user.uid}/#/dynamic">${card.user.uname}</a>：${card.item.content}</p>
+        <p><a href="https://t.bilibili.com/${card.item.dynamic_id}">阅读原文</a></p>
+      `;
+    case CARD_TYPES.REPOST:
+      return `
+        <p><a href="https://space.bilibili.com/${card.user.uid}/#/dynamic">${card.user.uname}</a>：${card.item.content}</p>
+      `;
+    default:
+      return "<p>不支持的动态内容</p>";
+  }
+  // tslint:enable:max-line-length
+};
 
 class BiliDynamic extends Service {
   constructor(store: Store) {
     super(new ApiClient(
-      "265D5310-DC29-4F35-9E07-F6B74F6BEF26",
-      "哔哩哔哩 UP 主动态",
-      "测试中。配置信息请填写 uid。"), store);
+      "47BF7A7F-78DC-4B07-850F-9969080A0C19",
+      "哔哩哔哩关注动态",
+      "测试中。配置信息请填写 cookies。支持的动态类型：图片、文字、投稿、小视频、番剧、文章。"), store);
   }
   public initialize = () =>
     this.client.register()
-  public mapChannelToMessages = async (uid: string) => {
+  public mapChannelToMessages = async (config: string) => {
+    const cookie = parseCookie(config);
+    const uid = cookie.DedeUserID;
     const response = await axios({
       method: "get",
-      url: `https://api.vc.bilibili.com/dynamic_svr/v1/dynamic_svr/space_history?host_uid=${uid}`,
+      url: `https://api.vc.bilibili.com/dynamic_svr/v1/dynamic_svr/dynamic_new?uid=${uid}&type=${CARD_TYPES.ALL}`,
       headers: {
         "User-Agent": SANDRA_CRAWLER_UA,
-        "Referer": `https://space.bilibili.com/${uid}/`,
+        "Referer": "https://t.bilibili.com",
+        "Origin": "https://t.bilibili.com",
+        // tslint:disable-next-line:max-line-length
+        "Cookie": `DedeUserID=${cookie.DedeUserID}; DedeUserID__ckMd5=${cookie.DedeUserID__ckMd5}; SESSDATA=${cookie.SESSDATA}`,
       },
       // tslint:disable-next-line:no-shadowed-variable
       transformResponse: [(data) => data],
     });
     const data: any[] = JSONbig.parse(response.data).data.cards;
-    const feed = {
-      title: `${data[0].desc.user_profile.info.uname} 的 bilibili 动态 `,
-      link: `https://space.bilibili.com/${uid}/#/dynamic`,
-      description: `${data[0].desc.user_profile.info.uname} 的 bilibili 动态 `,
-      item: data.map((item) => {
-        const parsed = JSONbig.parse(item.card);
-        // tslint:disable-next-line:no-shadowed-variable
-        const data = parsed.item || parsed;
 
-        // img
-        let imgHTML = "";
-        if (data.pictures) {
-          // tslint:disable-next-line:prefer-for-of
-          for (let i = 0; i < data.pictures.length; i++) {
-            imgHTML += `<img referrerpolicy="no-referrer"src="${data.pictures[i].img_src}">`;
-          }
+    return data
+      .map(item => {
+        // tslint:disable:max-line-length
+        const card = JSONbig.parse(item.card);
+        switch (item.desc.type) {
+          case CARD_TYPES.VIDEO:
+            return new Message(
+              `@${item.desc.user_profile.info.uname} 有新投稿视频`,
+              card.dynamic,
+              mapCardToContent(item.desc.type, card),
+              new Date(item.desc.timestamp * 1000),
+            );
+          case CARD_TYPES.CLIP:
+            return new Message(
+              `@${item.desc.user_profile.info.uname} 有新的小视频`,
+              card.item.description,
+              mapCardToContent(item.desc.type, card),
+              new Date(item.desc.timestamp * 1000),
+            );
+          case CARD_TYPES.DRAMA:
+            return new Message(
+              `${card.title} 更新了第 ${card.new_ep.index} 集`,
+              `${card.new_ep.index_title}`,
+              mapCardToContent(item.desc.type, card),
+              new Date(item.desc.timestamp * 1000),
+            );
+          case CARD_TYPES.BANGUMI:
+            return new Message(
+              `${card.apiSeasonInfo.title} 更新了第 ${card.index} 集`,
+              `${card.index_title}`,
+              mapCardToContent(item.desc.type, card),
+              new Date(item.desc.timestamp * 1000),
+            );
+          case CARD_TYPES.ARTICLE:
+            return new Message(
+              `@${item.desc.user_profile.info.uname} 有新专栏文章`,
+              `${card.title} ${card.summary}`,
+              mapCardToContent(item.desc.type, card),
+              new Date(item.desc.timestamp * 1000),
+            );
+          case CARD_TYPES.PIC:
+            return new Message(
+              `@${item.desc.user_profile.info.uname} 有新动态`,
+              `${card.item.description}`,
+              mapCardToContent(item.desc.type, card),
+              new Date(item.desc.timestamp * 1000),
+            );
+          case CARD_TYPES.WORD:
+            return new Message(
+              `@${item.desc.user_profile.info.uname} 有新动态`,
+              `${card.item.content}`,
+              mapCardToContent(item.desc.type, card),
+              new Date(item.desc.timestamp * 1000),
+            );
+          case CARD_TYPES.REPOST:
+            return new Message(
+              `@${item.desc.user_profile.info.uname} 有新动态`,
+              `${card.item.content}`,
+              mapCardToContent(item.desc.type, card) + mapCardToContent(item.desc.orig_type, JSONbig.parse(card.origin)),
+              new Date(item.desc.timestamp * 1000),
+            );
+          default:
+            return null;
         }
-        if (data.pic) {
-          imgHTML += `<img referrerpolicy="no-referrer"src="${data.pic}">`;
-        }
-
-        // link
-        let link = "";
-        if (data.dynamic_id) {
-          link = `https://t.bilibili.com/${data.dynamic_id}`;
-        } else if (data.aid) {
-          link = `https://www.bilibili.com/video/av${data.aid}`;
-        } else if (data.video_playurl) {
-          link = `https://vc.bilibili.com/video/${data.id}`;
-        } else if (data.id) {
-          link = `https://h.bilibili.com/${data.id}`;
-        } else if (data.sketch.sketch_id) {
-          link = `https://t.bilibili.com/${item.desc.dynamic_id}`;
-        }
-
-        return {
-          title: data.title || data.description || data.content || (data.vest && data.vest.content),
-          // tslint:disable-next-line:max-line-length
-          description: `${data.desc || data.description || data.content || data.summary || (data.vest && data.vest.content) + (data.sketch && data.sketch.title)}${imgHTML} `,
-          pubDate: new Date(item.desc.timestamp * 1000).toUTCString(),
-          link,
-        };
-      }),
-    };
-    return feed.item
-      .map(item => new Message(
-        feed.title,
-        item.description,
-        `${item.description}<p><a href="${item.link}">查看原文</a></p>`,
-        new Date(item.pubDate),
-      ));
+        // tslint:enable:max-line-length
+      })
+      .filter(item => item !== null) as Message[];
   }
 }
 
